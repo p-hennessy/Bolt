52596e6692544339734ee2280b4cc160bd435db7
Patrick Henenssy <ph7vc@mail.missouri.edu>
2014-11-15 19:58:19 -0500

    Added Trivia class to handle all the bot logic. Added main loop logic. Need to add answer checker, connect to the response hook, and implement user database for awarding points

--- trebek.py
+++ trebek.py
@@ -1,32 +1,109 @@
 #!/usr/bin/python
 
+# ************************** #
+#        Trebek v 1.0        #
+#      Slack Trivia Bot	     #
+#  		                    #
+#             by             #
+#      Patrick Hennessy      #
+#                            #
+#      github.com/ph7vc	     #
+# ************************** #
+
 import sys
 import BaseHTTPServer
 import urlparse
+import simplejson as json
+import time
 
-expectedRequestKeys = ['user_id', 
-							'channel_name', 
-							'timestamp', 
-							'team_id', 
-							'channel_id', 
-							'token', 
-							'text', 
-							'service_id', 
-							'team_domain', 
-							'user_name']
+expectedRequestKeys = ['user_id','channel_name','timestamp','team_id','channel_id','token','text','service_id','team_domain','user_name']
 
 config = {}
-answer = ""
+questions = {}
+answerFound = False
+trebek = None
 
-class color():
-	reset  = '\033[0m' 
-	red  = '\033[31m'  	
-	green  = '\033[32m'  	
-	orange  = '\033[33m' 	
-	blue  = '\033[34m'  	
-	purple  = '\033[35m'  	
-	cyan  = '\033[36m'  	
-	gray = '\033[37m'  	
+def main():
+	global trebek 
+	trebek = Trivia()
+
+	while True:
+		trebek.startTimer()		
+		trebek.askQuestion()
+		
+		while trebek.getElapsedTime() < 5:
+			trebek.listenForAnswers()
+			
+			if (answerFound):
+				print "Answer Found!"
+				break
+
+ 			if(int(trebek.getElapsedTime()) == 15):
+ 				print "Hint 1"
+ 			elif(int(trebek.getElapsedTime()) == 45):
+ 				print "Hint 2"
+ 				
+		if(answerFound):
+			trebek.givePoints()
+		else:
+			trebek.giveAnswer()
+
+		trebek.delay(5)		
+		
+class Trivia():
+	currentQuestion = 0
+	questionSet = "pokemon"
+	timer = time.time()	
+	
+	def __init__(self):
+		global httpd
+		httpd = BaseHTTPServer.HTTPServer(('', 69), RequestHandler)
+		httpd.socket.settimeout(1)
+
+		prettyPrint("Starting Trebek 1.0")	
+	
+		loadConfig()
+		loadQuestions()
+	
+	def startTimer(self):
+			self.timer = time.time()
+
+	def getElapsedTime(self):
+			return time.time() - self.timer	
+
+	def delay(self, seconds):
+		time.sleep(seconds)	
+	
+	def askQuestion(self):
+		global answerFound
+		answerFound = False
+
+		prettyPrint(color.white + "> Asking question: " + color.reset + questions[self.questionSet][self.currentQuestion]["question"])
+		
+	def listenForAnswers(self):
+		return httpd.handle_request()
+		
+	def checkAnswer(self, answer):
+		print "checking for answer"
+		return False
+
+	def getNextQuestion(self):
+		if(len(questions["pokemon"]) - 1 == self.currentQuestion):
+			self.currentQuestion = 0
+	
+		else:
+			self.currentQuestion += 1
+
+	def givePoints(self):
+		self.getNextQuestion()
+		print "10 pints!"	
+	
+	def giveAnswer(self):
+		self.getNextQuestion()	
+		print "Answer was xxx..."
+	
+	def giveHint(self):
+		print "giving hint"
 	
 	
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -44,7 +121,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 		if set(expectedRequestKeys).issubset(post.keys()):
 			self.send_response(200)
 		else:
-			self.send_response(418)
+			self.send_response(418) # Tell the client about your teapot-ness
 			return
 		
 		# Check security token
@@ -53,40 +130,51 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 		self.end_headers()
 		
-		checkForAnswer(post['user_id'], post['user_name'], post['text'])
+		global answerFound
+		answerFound = trebek.checkAnswer(post["text"])		
+		
 
-def checkForAnswer(uid, uname, msg):
-    print "check answerr"	
-
-def listenForAnswers():
-	httpd.handle_request()
-
+# Load config from a file storing into global dict
 def loadConfig():
+	prettyPrint("Loading config...")
+	
 	file = open("trebek.config", "r+")
 	lines = file.read()
 	file.close()
 		
 	for index in lines.split("\n"):
-
 		split = index.split("=")
 		
 		if len(split) == 2:		
 			config[split[0]] = split[1]
-			
-def init():
-	global httpd
-	httpd = BaseHTTPServer.HTTPServer(('', 69), RequestHandler)
-	httpd.socket.settimeout(2)
-	loadConfig()
 
-def main():
-	init()
-		
-	while True:
-		question = "Which Pokemon do you start with in Pokemon Yellow version?"
-		hint = "p _ k a _ h _" 
-		listenForAnswers()
+# Load the questions from a file storing into global dict
+def loadQuestions():
+	prettyPrint("Loading questions from: \"" + config["questions"] + "\"")
 	
+	file = open(config["questions"], "r")
+	
+	global questions
+	questions = json.load(file)
+	
+	file.close()
+
+# Aux functions for output stuff
+class color():
+	reset  = '\033[0m'
+	gray = '\033[1;30m' 
+	red  = '\033[1;31m'  	
+	green  = '\033[1;32m'  	
+	yellow  = '\033[1;33m' 	
+	blue  = '\033[1;34m'  	
+	purple  = '\033[1;35m'  	
+	cyan  = '\033[1;36m'  	
+	white = '\033[1;37m'  
+		
+def prettyPrint(msg, tabLevel=0):
+	print "\t" + color.white + "[" + color.yellow + "Trebek v1.0" + color.white + "] " + color.reset + ("\t" * tabLevel) + msg
+	
+# Startup function	
 if __name__ == '__main__':
 	try:
 		main()
@@ -107,3 +195,4 @@ if __name__ == '__main__':
 
 
 
+# ************************** #
