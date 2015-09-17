bc3e3aaea026deb31ad053a6c3d6c3535c8ee49b
Patrick Hennessy <phennessy517@gmail.com>
2015-09-16 23:04:07 -0500

    Added threading for recieving web socket messages. Implementing message and command buffer for realtime message consumption

--- bot.py
+++ bot.py
@@ -10,137 +10,29 @@ import random
 import re
 import sys
 
-        
-def main():        
-        
-    Regis = Trivia()
-    
-    Regis.login("xoxb-6375106610-PiKFtyOj00bOBqTbaqCskYBb")
-    Regis.say("Regis Philbot 2.0 Initializing...")
-    Regis.listen(100)
-    
-    Regis.logout()
+def main():
 
-class Bot():  
+    connection = SlackConnection("xoxb-6375106610-PiKFtyOj00bOBqTbaqCskYBb")
+    connection.connect()
+
+    try:
+        while True:
+            msg = connection.getMessage()
+
+            if(msg):
+                print "MESSAGE: " + str(msg)
+
+    except KeyboardInterrupt:
+        connection.killMessageThread()
+        connection.disconnect()
+
+class Bot():
+
     def __init__(self):
-        self.connection = None
-        self.session = None
-        
-        self.userInfo = {}
-        self.timer = Timer()
-        
-    def login(self, token):
-        self.connection = SlackConnection(token)
-        self.userInfo = self.connection.getBotData()
-
-    def logout(self):
-        self.connection.disconnect()
-        sys.exit(0)
-    
-    def say(self, message, channel="general"):
-        self.connection.emit(channel, message)
-    
-    def ping(self):
-        print self.connection.ping()
-        
-    def listen(self, duration, onMessage=None):
-        self.timer.start()
-        
-        if not onMessage:
-            onMessage = getattr(self, "_onMessage")
-        
-        self.listening = True
-        while(self.timer.getElapsedTime() < duration and self.listening):
-            message = self.connection.recv()
-
-            rlist, _, _ = select([sys.stdin], [], [], 0)
-            if rlist:
-                input = sys.stdin.readline()
-                self.parseCommand(input)
-            
-            if(message):
-                message = self.parseMessage(message)             
-                
-                if(message):
-                    onMessage(message)
-                    
-        self.listening = False
-     
-    def _onMessage(self, message):
-        print message
-            
-    def parseMessage(self, messageData):    
-        if("type" in messageData.keys() and messageData["type"] == "message"): 
-            sender = messageData["user"]
-            message = messageData["text"]
-
-            # Don't care about own messages
-            if(sender == self.userInfo["id"]):
-                return
-
-            # If message is a command
-            elif(re.match(r"^regis\ ", message)):
-                self.parseCommand(re.sub(r"^regis\ ", "", message))
-            
-            else:        
-                return messageData
-                
-    def parseCommand(self, command):
-        args = []
-        
-        for arg in re.findall(r'(?:"[^"]*"|\'[^\']*\'|[^\s\'"])+', command):
-            arg = re.sub(r'^["\']|["\']$', '', arg)
-            args.append(arg)
-            
-        print args
-            
-        try:
-            commandInvocation = getattr(self, args[0])
-            commandInvocation(*args[1:])
-        except AttributeError:
-            return
-        except TypeError:
-            #self.say("[Command Error]: " + commandInvocation.__doc__ + " " + str(len(args[1:])) + " were given.")
-            return
-            
-        
-class Trivia(Bot):
-    def __init__(self):
-        self.timer = Timer()
-        self.questions = None
-        self.running = False
-    
-    def start(self):
-        if not self.running:
-            self.running = True
-                        
-        while self.running:
-            
-            # Random Pause            
-            # Ask Question
-            self.say("Heres a question!")
-            self.listen(10, self._checkAnswer)
-
-            # Listen for answer, giving hint
-            # Give a hint
-            
-    def stop(self):        
-        """ Trivia.stop() | Stops playing trivia. Takes 0 arguments. """
-        self.running = False
-    
-    def _askQuestion(self, question):
         pass
-    
-    def _checkAnswer(self, question):
-        if(question and question["text"] == "test"):
-            self.listening = False
-            return True
-        else:
-            return False
 
-    
-    
-    
+    def login(self):
+        
 
 if __name__ == '__main__':
 	try:
--- lib/SlackClient.py
+++ lib/SlackClient.py
@@ -1,114 +1,122 @@
 import websocket
 import json
 import time
+import threading
 
 from ssl import *
 from lib.SlackAPI import SlackAPI
-from lib.Logger import *
-
-class SlackSession():
-    def __init__(self):
-        pass
-
 
 class SlackConnection():
+
     def __init__(self, token):
-        
-        self.slackAPI   = SlackAPI()
-        self.log        = Logger()
+
+        self.slackAPI   = SlackAPI()    # Python implemented interface for hitting Slack API
         self.token      = token
-        
+
         self.socket     = None
         self.socketURL  = None
 
         self.loginData  = None
         self.channels   = []
-        
+
+        self.messageThread = None
+        self.messageBuffer = []
+
         self.connected  = False
-        self.connect()
-    
+
     def ping(self):
         return self.socket.send(json.dumps({"type":"ping"}))
-    
+
     def connect(self):
+        # Initialize Slack websocket by polling the [rtm.start] API method with authentication token
         reply = self.slackAPI.rtm.start(self.token)
-                
+
         if(reply["ok"]):
             self.connected = True
             self.parseLoginData(reply)
-            
+
             try:
                 self.socket = websocket.create_connection(self.socketURL)
-                self.socket.sock.setblocking(0) 
+                self.socket.sock.setblocking(0)
             except:
-                raise SlackConnectionError # Could not establish a connection   
+                raise SlackConnectionError # Could not establish a connection
+
+            self.messageThread = threading.Thread(target=self.recv)
+            self.messageThread.start()
         else:
             pass # Unable to login
 
     def disconnect(self):
         self.socket.close()
         self.connected = False
-    
+
     def emit(self, channel, message):
         channelID = self.getChannelID(channel)
-        
+
         if not channelID:
             self.log.error("Channel \"" + channel + "\" does not exist")
         else:
             self.socket.send(json.dumps({"type": "message", "channel": channelID, "text": message}))
-        
+
     def recv(self):
-        data = ""
         while True:
             try:
-                data += self.socket.recv()
+                data = self.socket.recv()
+
+                data = data.rstrip()
+                data = json.loads(data)
+
+                self.messageBuffer.append(data)
             except SSLError:
-                return None
+                pass
             except websocket._exceptions.WebSocketConnectionClosedException:
-                self.connect()
-                break
-            
-            data = data.rstrip()
-            data = json.loads(data)
-            
-            return data
-        
+                print "Disconnected while recv..."
+
+    def getMessage(self):
+        if(len(self.messageBuffer) > 0 ):
+            returnBuffer = self.messageBuffer
+            self.messageBuffer = []
+
+            return returnBuffer
+        else:
+            return None
+
     def parseLoginData(self, loginData):
-        
+
         self.loginData  = loginData
         self.socketURL  = self.loginData["url"]
-                
+
         self.parseChannelData(loginData["channels"])
         self.parseChannelData(loginData["groups"])
         self.parseChannelData(loginData["ims"])
 
     def parseChannelData(self, channels):
-         for channel in channels:    
+         for channel in channels:
             if "name" not in channel.keys():
                 channel["name"] = channel["id"]
-            
+
             if "members" not in channel.keys():
                 channel["members"] = []
-        
+
             self.channels.append(Channel(channel["name"], channel["id"], channel["members"]))
-    
+
     def getChannelID(self, searchTerm):
         for channel in self.channels:
             if(searchTerm.lower() == channel.name.lower() or searchTerm == channel.id):
                 return channel.id
-        
+
         return False
-    
+
     def getBotData(self):
         return {"domain": self.loginData["team"]["domain"], "username": self.loginData["self"]["name"], "id": self.loginData["self"]["id"]}
 
     def getUserData(self, id):
-        return self.slackAPI.users.info(self.token, id)    
-        
-        
+        return self.slackAPI.users.info(self.token, id)
+
+
 class SlackConnectionError(Exception):
-    pass        
-        
+    pass
+
 class SlackLoginError(Exception):
     pass
 
@@ -123,5 +131,3 @@ class Channel():
     def send_message(self, message):
         message_json = {"type": "message", "channel": self.id, "text": message}
         #self.server.send_to_websocket(message_json)
-        
-        
