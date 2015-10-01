16f7b2596c27a14638cf68c1e567666ec198f436
Patrick Hennessy <phennessy517@gmail.com>
2015-10-01 11:25:59 -0500

    Added handler for connection reset by peer. Should stop socket-based threads

--- connectors/Discord.py
+++ connectors/Discord.py
@@ -116,7 +116,7 @@ class Discord(Connector):
         self.messageConsumerThread.join()
 
         self.socket.close()
-        self.logger.info("Succesful logout from Discord")
+        self.logger.info("Disconnected from Discord")
 
     def send(self, channel, message, mentions=[]):
         self.logger.debug("Sending message to channel " + channel)
@@ -165,13 +165,27 @@ class Discord(Connector):
     def keepAlive(self):
         self.logger.debug("Spawning keepAlive thread at interval: " + str(self.heartbeatInterval))
 
+        startTime = time.time()
+
         while self.connected:
-            time.sleep((self.heartbeatInterval / 1000) - 1)
-            self.writeSocket({"op":1,"d": time.time()})
-            self.logger.debug("KeepAlive")
+            now = time.time()
+
+            if((now - startTime) >= (self.heartbeatInterval/1000) - 1):
+                self.writeSocket({"op":1,"d": time.time()})
+                self.logger.debug("KeepAlive")
+
+                startTime = time.time()
+
+            time.sleep(0.5)
 
     def writeSocket(self, data):
-        self.socket.send(json.dumps(data))
+        try:
+            self.socket.send(json.dumps(data))
+        except socket_error as e:
+            if e.errno == 104:
+                self.logger.warning("Connection reset by peer")
+                self.core.threadPool.queueTask(self.handleInteruption)
+                self.connected = False
 
     def readSocket(self):
         data = ""
@@ -192,10 +206,22 @@ class Discord(Connector):
                     return None
                 raise
             except socket_error as e:
+                # Raised when connection reset by peer
+                if e.errno == 104:
+                    self.logger.warning("Connection reset by peer")
+                    self.core.threadPool.queueTask(self.handleInteruption)
+                    self.connected = False
+
+                    return None
+
                 # Raised when send buffer is full; we must try again
                 if e.errno == 11:
                     return None
                 raise
+
+    def handleInteruption(self):
+        self.disconnect()
+        self.connect()
 
     def parseMessageData(self, message):
         type = content = sender = channel = content = timestamp = None
