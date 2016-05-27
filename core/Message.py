daaac9acb71d8c19e2a0a052f62effe742b4c5ac
Patrick Hennessy <phennessy517@gmail.com>
2016-05-26 21:53:29 -0400

    Removed envelope concept; pep8ify

--- core/Message.py
+++ core/Message.py
@@ -1,5 +1,5 @@
 """
-    Class Name : MessageParser
+    Class Name : Message
 
     Description:
         Simple class to standardize message data
@@ -13,40 +13,36 @@
         by the Free Software Foundation
 """
 import time
-import shlex
 import re
 
 class messageType:
     MESSAGE = 0
     PRESSENCE = 1
 
-class Envelope():
-    def __init__(self, user, channel):
-        self.sender = sender
-        self.channel = channel
-
 class Message():
-    def __init__(self, type, sender_id, channel_id, content=None, sender_name=None, timestamp=time.time()):
+    def __init__(self, type, sender_id, channel_id, content="", mentions=[], sender_name=None, timestamp=time.time()):
         self.type = type
         self.timestamp = timestamp
         self.sender = sender_id
         self.sender_name = sender_name
         self.channel = channel_id
         self.content = content
-        self.__match_obj = None
-        self.__match = None
+        self.mentions = mentions
+
+        self._match_obj = None
+        self._match = None
 
     @property
     def arguments(self):
-        if(self.__match_obj):
-            return self.__match_obj.groups()
+        if(self._match_obj):
+            return self._match_obj.groups()
         else:
             return []
 
     @arguments.setter
     def arguments(self, match):
-        self.__match_obj = match
-        self.__match = match.groups()
+        self._match_obj = match
+        self._match = match.groups()
 
     def __call__(self):
-        return Envelope(self.sender, self.channel)
+        return (self.sender, self.channel)
