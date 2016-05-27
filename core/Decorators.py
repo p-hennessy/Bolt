f5c5f66482e85ecb33c4d8a56b3a28967c02c002
Patrick Hennessy <phennessy517@gmail.com>
2016-05-26 21:51:03 -0400

    Implemented ttl_cache decorator for frequent requests

--- core/Decorators.py
+++ core/Decorators.py
@@ -1,17 +1,43 @@
-# Adds attributes to decorated methods that will be picked up when plugin is loaded
-def command(trigger, access=0, useDefaultTrigger=True):
+import time
+
+class ttl_cache(object):
+    def __init__(self, ttl):
+        self.cache = {}
+        self.ttl = ttl
+
+    def __call__(self, callback):
+        def wrapper(*args):
+            now = time.time()
+            try:
+                value, last_update = self.cache[args]
+                if self.ttl > 0 and now - last_update > self.ttl:
+                    raise AttributeError
+
+                return value
+            except (KeyError, AttributeError):
+                value = callback(*args)
+                self.cache[args] = (value, now)
+
+                return value
+            except TypeError:
+                return callback(*args)
+        return wrapper
+
+
+def command(pattern, access=0, trigger="", silent=False):
     def decorate(callback):
         def wrapper(self, msg):
-            self.logger.info("Command [{}] invoked by [{}][UID:{}] Raw:{}".format(callback.__name__, msg.sender_name, msg.sender, msg.content))
             return callback(self, msg)
 
         if not hasattr(wrapper, 'is_command'):
             wrapper.__name__ = callback.__name__
             wrapper.__doc__ = callback.__doc__
             setattr(wrapper, 'is_command', True)
+            setattr(wrapper, 'pattern', pattern)
+            setattr(wrapper, 'callback', callback)
             setattr(wrapper, 'trigger', trigger)
             setattr(wrapper, 'access', access)
-            setattr(wrapper, 'useDefaultTrigger', useDefaultTrigger)
+            setattr(wrapper, 'silent', silent)
 
         return wrapper
     return decorate
