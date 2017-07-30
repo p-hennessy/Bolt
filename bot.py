b80043d5e24832f89b56aee1e7fd6d51ef9d1715
Patrick Hennessy <phennessy517@gmail.com>
2017-07-30 16:11:09 -0400

    updated bot py

--- bot.py
+++ bot.py
@@ -14,14 +14,21 @@
         under the terms of the GNU General Public License v3; as published
         by the Free Software Foundation
 """
-from core.Core import Bot
-from connectors.Discord import Discord
+from arcbot.Core import Bot
+import sys
 
-arcbot = Bot()
+arcbot = Bot("TOKEN HERE")
 arcbot.trigger = 'arcbot '
-arcbot.connector = Discord(arcbot, token="")
 
-for plugin in arcbot.discover_plugins('plugins'):
+for plugin in arcbot.plugin.discover('plugins'):
     arcbot.plugin.load(plugin)
 
 arcbot.connect()
+
+try:
+    arcbot.run_forever()
+except KeyboardInterrupt:
+    arcbot.disconnect()
+    arcbot.exit()
+
+    sys.exit(0)
