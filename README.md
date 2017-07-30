2c5e600290d8af10b1648af5b65a348042335284
Patrick Hennessy <phennessy517@gmail.com>
2017-07-30 16:05:29 -0400

    Strip out a lot of unnessessary abstractions. Use full discord event API, new event system, a real help command, and much more..

--- README.md
+++ README.md
@@ -1,46 +1,5 @@
 # About Arcbot
 Arcbot is an extensible chatbot written entirely in Python, inspired by Err and Hubot projects.
-The goal of Arcbot is to provide a simple to extend bot that can be used with many different chat services like Slack, Hipchat, Discord, IRC, Campire, etc.
+The goal of Arcbot is to provide a simple to extend bot.
 
 Arcbot is open source software and released under the GPL v3 license. See LICENCE.txt for more info.
-
-# Installing / Running
-Everything about Arcbot is self contained, meaning that the entire bot is shipped and you run it from there.
-
-### Running
-1. Clone this repo
-2. Fill out the `conf/settings.conf` file with your config settings
-3. You must install the packages found in `REQUIREMENTS.txt`. Try doing `pip -r REQUIREMENTS.txt`
-4. Run the entry point `bot.py` with Python 3
-5. You're good to go!
-
-# Bot Architecture
-
-### Connectors
-**Connectors** are what allow Arcbot to talk to the chat server. Every connector is subclassed from `core/connector.py`, which is the interface that the bot uses for the connector.
-
-Since each chat provider has a different API, the internals of the connector will likely vary greatly; but must implement the abstract methods found in `core/connector.py` in order to function with the bot.
-
-Currently, Arcbot only supports the unoffical Discord API connector. A full API has yet to be released for Discord, this connector is based on reverse engineering.
-
-More connector support is coming soon: (Slack, Hipchat, Gitter)
-
-### Plugin System
-Using the power of Python, Arcbot gets all of it's functionality from plugins. The plugin system takes care of all the annoying code behind the scenes, so the code in for plugin is minimal. A sample plugin can be found at `plugins/Sample.py`, a plugin can be either a single python file or a folder with many files (in this case the main class should be in __init__.py file).
-
-Arcbot also ships with a few plugins to get you started.
-
-### ACL's
-**Access control lists** are a way to enforce security policies for users of the bot. You may not want everyone in the channel to be able to disable plugins, or have the bot start Jenkins builds, so this is a way to assign user access and restrict what commands they can use.
-
-### Multi-threaded Design
-Arcbot makes use of thread pools to do it's dirty work. The command and event systems both will offload their work to threaded workers; to allow the bot to handle a very large load at once. This has the added benefit of being extremely responsive. This feature is tuneable in `conf/settings.py`; so you can set it up to best utilize your particular CPU and RAM needs.
-
-### More features coming soon
-Arcbot is still in active development, and not all design choices have been made. There is still a host of features I want to add before considering it stable; things like: 
-* Web hook listeners
-* Web based UI to manage the bot
-* Connectors for other chat providers
-
-# Credits
-Credit must be given to the developers behind Hubot and Err projects; a lot of this bot was inspired by those projects.
--- plugins/Manage.py
+++ plugins/Manage.py
@@ -14,75 +14,176 @@
         by the Free Software Foundation
 """
 
-from core.Plugin import Plugin
-from core.Decorators import *
+from arcbot.Plugin import Plugin
+from arcbot.Decorators import *
+from arcbot.Discord import events
+from arcbot.Discord import api
+from arcbot.Discord import embed_intent
 
 import time
-from tabulate import tabulate
+import re
+from collections import defaultdict
+from datetime import timedelta
 
 class Manage(Plugin):
     def activate(self):
-        self.login_time = None
-        self.session_time = None
+        self.login_time = 1466978500
+        self.session_time = 1466978500
 
-    @command("^ping", access=50)
-    def ping(self, msg):
-        self.say(msg.channel, "Pong")
+    @command("^ping")
+    def ping(self, event):
+        """
+            Description: Basic command to verfiy bot can respond to chat messges
+            Usage: arcbot ping
+        """
+        self.say(event.channel_id, "Pong")
 
-    @command("^trigger$", trigger="?", access=50)
-    def trigger(self, msg):
-        self.say(msg.channel, "My default trigger is `" + self.core.trigger + "`")
+    @command("^trigger$", trigger="?")
+    def trigger(self, event):
+        """
+            Description: Prints out the default trigger for the bot
+            Usage: ?trigger
+        """
+        self.say(event.channel_id, f"My default trigger is `{self.core.trigger}`")
 
-    @command("^list plugins$", access=50)
-    def list_plugins(self, msg):
+    @command("^(help|commands)$")
+    def help(self, event):
+        """
+            Description: Well this is meta. Prints out all the commands and plugins the bot knows about
+            Usage: arcbot [help, commands]
+        """
+        start = time.monotonic()
+        commands = defaultdict(list)
+
+        for _, command in self.core.command.commands.items():
+            command_class = type(command.callback.__self__).__name__
+            command_name = command.callback.__name__
+
+            commands[command_class].append(command_name)
+
+        fields = []
+
+        for name, items in commands.items():
+            fields.append({
+                "name": name + ":",
+                "value": "\n".join([f"`{i}`" for i in items])
+            })
+
+
+        embed = {
+            "title": f":information_source: {self.core.name} Help",
+            "description": "**Usage**:\nTo get a help page, type: ```html\narcbot help <plugin>.<command>```",
+            "color": embed_intent.INFO,
+            'fields': fields,
+            "footer": {
+              "text": f"⏱ {timedelta(seconds=time.monotonic()-start)}"
+            }
+        }
+
+        self.say(event.channel_id, embed=embed)
+
+
+    @command("help ([A-Za-z]+).([A-Za-z]+)")
+    def help_page(self, event):
+        """
+            Description: Well this is meta. Prints out useful help messages
+            Usage: arcbot help <plugin>.<command>
+        """
+        start = time.monotonic()
+
+        for _, command in self.core.command.commands.items():
+            command_class = type(command.callback.__self__).__name__
+            command_name = command.callback.__name__
+
+            if command_class.lower() == event.arguments[1].lower() and command_name.lower() == event.arguments[2].lower():
+                help_name = f"plugins.{command_class}.{command_name}"
+                help_access_level = command.access
+                help_description = "Not documented"
+                help_usage = "Not documented"
+
+                if not command.callback.__doc__:
+                    self.say(event.channel_id, f"No help page found for `{command_class}.{command_name}`")
+                    return
+
+                for line in command.callback.__doc__.splitlines():
+                    line = line.strip("\n ")
+
+                    if re.search("Description:", line):
+                        help_description = line.split(":")[1]
+                    elif re.search("Usage:", line):
+                        help_usage = line.split(":")[1]
+
+                embed = {
+                    "title": f":information_source: {self.core.name} Help",
+                    "description": f"\n*{help_name}*\n\n**Description**\n{help_description}",
+                    "color": int("7289da", 16),
+                    'fields': [
+                        {'name': 'Usage', 'value': f"`{help_usage}`", "inline": True},
+                        {'name': 'Access', 'value': f"`{help_access_level}`", "inline": True}
+                    ],
+                    "footer": {
+                      "text": f"⏱ {timedelta(seconds=time.monotonic()-start)}"
+                    }
+                }
+
+                self.say(event.channel_id, embed=embed)
+                break
+
+
+    @command("^list plugins$")
+    def list_plugins(self, event):
+        start = time.monotonic()
+
+        plugins = self.core.plugin.list()
+        plugins = sorted(plugins.items())
+
+        names = "".join([name + "\n" for name, meta in plugins])
+        statuses = "".join([meta['status'] + "\n" for name, meta in plugins])
+
+        embed = {
+            "color": int("7289da", 16),
+            'fields': [
+                {'name': 'Name', 'value': names, "inline": True},
+                {'name': 'Status', 'value': statuses, "inline": True}
+            ],
+            "footer": {
+              "text": f"⏱ {timedelta(seconds=time.monotonic()-start)}"
+            }
+        }
+
+        self.say(event.channel_id, embed=embed)
+
+    @command("^(enable|disable) plugin ([A-Za-z]+)$")
+    def manage_plugin(self, event):
         plugins = self.core.plugin.list()
 
-        table = []
-        for name, meta in sorted(plugins.items()):
-            table.append([name, meta['status']])
-
-        output = "**My plugins: **\n\n"
-        output += "`{}`".format(tabulate(table, headers=["Name", "Status"], tablefmt="psql", numalign="left"))
-
-        self.say(msg.channel, output)
-
-    @command("^(enable|disable|reload|status) plugin ([A-Za-z]+)$", access=1000)
-    def manage_plugin(self, msg):
-        plugins = self.core.plugin.list()
-        plugin = msg.arguments[1]
-        action = msg.arguments[0]
+        plugin = event.arguments[2]
+        action = event.arguments[1]
 
         if plugin in plugins:
             if action == "enable":
                 if plugins[plugin]['status'] == "Disabled":
                     self.core.plugin.load(plugin)
-                    self.say(msg.channel, "Enabled plugin: **{}**".format(plugin))
+                    self.say(event.channel_id, f"Enabled plugin: **{plugin}**")
                 else:
-                    self.say(msg.channel, "{} is already enabled".format(plugin))
+                    self.say(event.channel_id, f"{plugin} is already enabled")
 
             elif action == "disable":
                 if plugins[plugin]['status'] == "Enabled":
                     self.core.plugin.unload(plugin)
-                    self.say(msg.channel, "Disabled plugin: **{}**".format(plugin))
+                    self.say(event.channel_id, f"Disabled plugin: **{plugin}**")
                 else:
-                    self.say(msg.channel, "{} is already disabled".format(plugin))
-
-            elif action == "reload":
-                if not plugins[plugin]['status'] == "Disabled":
-                    self.core.plugin.reload(plugin)
-                    self.say(msg.channel, "Reloaded plugin: **{}**".format(plugin))
+                    self.say(event.channel_id, f"{plugin} is already disabled")
         else:
-            self.say(msg.channel, "I don't have a plugin by that name.")
+            self.say(event.channel_id, "I don't have a plugin by that name.")
 
-    @subscribe("connector.connect")
-    def on_connect(self, *args, **kwargs):
-        if self.login_time:
-            self.session_time = time.time()
-        else:
-            self.login_time = time.time()
 
-    @command("^uptime$", access=50)
-    def uptime(self, msg):
+    @command("^uptime$")
+    def uptime(self, event):
+        """
+            Description: Show how long bot has been connected
+            Usage: arcbot uptime
+        """
         uptime = time.time() - self.login_time
 
         def readable_time(elapsed):
@@ -100,4 +201,4 @@ class Manage(Plugin):
 
             return readable
 
-        self.say(msg.channel, "I've been connected for: **" + readable_time(uptime) + "**")
+        self.say(event.channel_id, f"I've been connected for: **{readable_time(uptime)}**")
