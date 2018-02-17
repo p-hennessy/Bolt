04a386df2981f28b44169cd5107863048ae7ddcf
Patrick Hennessy <phennessy517@gmail.com>
2018-02-17 13:18:39 -0500

    Fix config liniting

--- arcbot/config.py
+++ arcbot/config.py
@@ -1,7 +1,8 @@
 import logging
 
+
 class Config():
     def __init__(self):
         self.name = "Arcbot"
         self.trigger = "arcbot "
-        self.log_level = logging.DEBUG
+        self.log_level = logging.INFO
