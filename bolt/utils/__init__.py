4e6d5edee7ec3f78641e3fc2db91f150fa92d3b0
Patrick Hennessy <phennessy517@gmail.com>
2019-06-21 01:17:21 -0500

    Add util function for converting from snakecase

--- bolt/utils/__init__.py
+++ bolt/utils/__init__.py
@@ -2,4 +2,5 @@ from bolt.utils.timer import Timer
 from bolt.utils.colors import Colors
 from bolt.utils.decorators import cache
 from bolt.utils.misc import readable_time
+from bolt.utils.misc import snakecase_to_camelcase
 from bolt.utils.logs import setup_logger
