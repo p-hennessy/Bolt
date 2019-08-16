d1ef5abfcc682237f74764aa73ba59e5f9c82086
Patrick Hennessy <phennessy517@gmail.com>
2019-08-16 02:24:46 -0500

    Linter

--- bolt/discord/models/__init__.py
+++ bolt/discord/models/__init__.py
@@ -1,69 +1,41 @@
-from bolt.discord.models.message import MessageType, Reaction, Attachment, Message
-from bolt.discord.models.user import PremiumType, User
-from bolt.discord.models.emoji import Emoji
-from bolt.discord.models.channel import (
-    ChannelType,
-    PermissionOverwrite,
-    Channel
-)
-from bolt.discord.models.embed import (
-    Footer,
-    Image,
-    Thumbnail,
-    Video,
-    Provider,
-    Author,
-    EmbedField,
-    Embed
-)
-from bolt.discord.models.guild import (
-    MessageNotificationLevel,
-    VerificationLevel,
-    ExplicitContentFilterLevel,
-    MFALevel,
-    GuildMember,
-    Role,
-    VoiceState,
-    ActivityType,
-    Activity,
-    Presence,
-    Ban,
-    VoiceRegion,
-    Emoji,
-    Guild
-)
+from bolt.discord.models.message import *
+from bolt.discord.models.user import *
+from bolt.discord.models.emoji import *
+from bolt.discord.models.channel import *
+from bolt.discord.models.embed import *
+from bolt.discord.models.guild import *
 
 __all__ = [
-    MessageType,
-    Reaction,
-    Attachment,
-    Message,
-    PremiumType,
-    User,
-    Emoji,
-    ChannelType,
-    PermissionOverwrite,
-    Channel,
-    Footer,
-    Image,
-    Thumbnail,
-    Video,
-    Provider,
-    Author,
-    EmbedField,
-    Embed,
-    MessageNotificationLevel,
-    VerificationLevel,
-    ExplicitContentFilterLevel,
-    MFALevel,
-    GuildMember,
-    Role,
-    VoiceState,
-    ActivityType,
-    Activity,
-    Presence,
-    Ban,
-    VoiceRegion,
-    Emoji,
-    Guild
+    "MessageType",
+    "Reaction",
+    "Attachment",
+    "Message",
+    "PremiumType",
+    "User",
+    "Emoji",
+    "ChannelType",
+    "PermissionOverwrite",
+    "Channel",
+    "Footer",
+    "Image",
+    "Thumbnail",
+    "Video",
+    "Provider",
+    "Author",
+    "EmbedField",
+    "Embed",
+    "MessageNotificationLevel",
+    "VerificationLevel",
+    "ExplicitContentFilterLevel",
+    "MFALevel",
+    "GuildMember",
+    "Role",
+    "VoiceState",
+    "ActivityType",
+    "Activity",
+    "Presence",
+    "Ban",
+    "VoiceRegion",
+    "Emoji",
+    "Guild"
 ]
